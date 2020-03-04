210da5b63ebf7a30ccac14223455eada096534be
irina_ubuntu <irina@me.com>
2020-03-04 17:48:11 +0300

    thymeleaf tests:  - model attributes  - redirection

---
+++ src/main/kotlin/com/mytests/spring/webflux/functionalkotlintest0/ThymeleafRouter.kt
@@ -0,0 +1,33 @@
+package com.mytests.spring.webflux.functionalkotlintest0
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.router
+import java.net.URI
+
+@Configuration
+class ThymeleafRouter {
+    @Bean
+    fun test1(): RouterFunction<ServerResponse> {
+        return router{
+            GET("/test1")
+            { val attr1 = "test1 passed"
+                ServerResponse.ok().render("test1", mapOf("test1_attr1" to attr1))}
+    }}
+
+    @Bean
+    open fun redirectionRouter(): RouterFunction<ServerResponse> {
+        val redirectToTest1 =
+                ServerResponse
+                        .temporaryRedirect(URI("/test1"))
+                        .build()
+
+        return router {
+            GET("/redirectMe") {
+                redirectToTest1 // also you can create request here
+            }
+        }
+    }
+}
